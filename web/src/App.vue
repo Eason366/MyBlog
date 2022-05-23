ccc2b81b5ff23cfc6c751c5bd3f38114f3d9920b
Eason-Wangy366 <754459450@qq.com>
2022-05-23 19:00:15 -0400

    footer header components

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,33 +1,24 @@
 <template>
   <a-layout>
-    <a-layout-header class="header" :style="{height:'64px', position: 'fixed', zIndex: 1, width: '100%'}">
-      <div class="logo"> Eason's Blog </div>
-      <a-menu
-          v-model:selectedKeys="selectedKeys1"
-          theme="dark"
-          mode="horizontal"
-          :style="{ lineHeight: '64px' }"
-      >
-        <a-menu-item key="1">nav 1</a-menu-item>
-        <a-menu-item key="2">nav 2</a-menu-item>
-        <a-menu-item key="3">nav 3</a-menu-item>
-      </a-menu>
-    </a-layout-header>
+    <the-header></the-header>
     <router-view/>
-    <a-layout-footer style="text-align: center">
-      Blog ©2022 Created by Eason
-    </a-layout-footer>
+    <the-footer></the-footer>
   </a-layout>
 </template>
 
+<script lang="ts">
+import { defineComponent } from 'vue';
+import TheHeader from '@/components/the-header.vue';
+import TheFooter from '@/components/the-footer.vue';
 
-<style>
-.logo {
-  width: 120px;
-  height: 31px;
-  float: left;
-  color: white;
-  font-size: 18px;
-}
+export default defineComponent({
+  name: 'app',
+  components: {
+    TheHeader,
+    TheFooter,
+  },
+});
+</script>
 
-</style>
+
+
---
+++ web/src/components/the-header.vue
@@ -0,0 +1,34 @@
+<template>
+  <a-layout-header class="header" :style="{height:'64px', position: 'fixed', zIndex: 1, width: '100%'}">
+    <div class="logo"> Eason's Blog </div>
+    <a-menu
+        v-model:selectedKeys="selectedKeys1"
+        theme="dark"
+        mode="horizontal"
+        :style="{ lineHeight: '64px' }"
+    >
+      <a-menu-item key="1">nav 1</a-menu-item>
+      <a-menu-item key="2">nav 2</a-menu-item>
+      <a-menu-item key="3">nav 3</a-menu-item>
+    </a-menu>
+  </a-layout-header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'the-header'
+});
+</script>
+
+<style>
+.logo {
+  width: 120px;
+  height: 31px;
+  float: left;
+  color: white;
+  font-size: 18px;
+}
+
+</style>
