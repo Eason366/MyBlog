d2abcde0e76f22c466c0d208ea8b9d48e0915ad0
Eason-Wangy366 <754459450@qq.com>
2022-05-23 02:08:54 -0400

    Add Blog Table, mybatis generator

---
+++ src/main/java/com/eason/blog/domain/Blog.java
@@ -0,0 +1,79 @@
+package com.eason.blog.domain;
+
+public class Blog {
+    private Long id;
+
+    private String name;
+
+    private Long category1Id;
+
+    private Long category2Id;
+
+    private String cover;
+
+    private Integer viewCount;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getCategory1Id() {
+        return category1Id;
+    }
+
+    public void setCategory1Id(Long category1Id) {
+        this.category1Id = category1Id;
+    }
+
+    public Long getCategory2Id() {
+        return category2Id;
+    }
+
+    public void setCategory2Id(Long category2Id) {
+        this.category2Id = category2Id;
+    }
+
+    public String getCover() {
+        return cover;
+    }
+
+    public void setCover(String cover) {
+        this.cover = cover;
+    }
+
+    public Integer getViewCount() {
+        return viewCount;
+    }
+
+    public void setViewCount(Integer viewCount) {
+        this.viewCount = viewCount;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", name=").append(name);
+        sb.append(", category1Id=").append(category1Id);
+        sb.append(", category2Id=").append(category2Id);
+        sb.append(", cover=").append(cover);
+        sb.append(", viewCount=").append(viewCount);
+        sb.append("]");
+        return sb.toString();
+    }
+}
