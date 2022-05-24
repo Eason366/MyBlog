b6bc542d14cee75b2febbd1260fcd19ae783cc43
Eason-Wangy366 <754459450@qq.com>
2022-05-24 05:03:37 -0400

    Add AOP

---
+++ src/main/java/com/eason/blog/aspect/LogAspect.java
@@ -0,0 +1,81 @@
+package com.eason.blog.aspect;
+
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.support.spring.PropertyPreFilters;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+@Aspect
+@Component
+public class LogAspect {
+
+    private final static Logger LOG = LoggerFactory.getLogger(LogAspect.class);
+
+    /** Pointcut */
+    @Pointcut("execution(public * com.eason.*.controller..*Controller.*(..))")
+    public void controllerPointcut() {}
+
+    @Before("controllerPointcut()")
+    public void doBefore(JoinPoint joinPoint) throws Throwable {
+
+        // Start printing request log
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        Signature signature = joinPoint.getSignature();
+        String name = signature.getName();
+
+        // print request information
+        LOG.info("------------- Begin -------------");
+        LOG.info("Request Address: {} {}", request.getRequestURL().toString(), request.getMethod());
+        LOG.info("Method: {}.{}", signature.getDeclaringTypeName(), name);
+        LOG.info("Remote Address: {}", request.getRemoteAddr());
+
+        // print request parameters
+        Object[] args = joinPoint.getArgs();
+        // LOG.info("Request Parameters: {}", JSONObject.toJSONString(args));
+
+        Object[] arguments  = new Object[args.length];
+        for (int i = 0; i < args.length; i++) {
+            if (args[i] instanceof ServletRequest
+                    || args[i] instanceof ServletResponse
+                    || args[i] instanceof MultipartFile) {
+                continue;
+            }
+            arguments[i] = args[i];
+        }
+        // Excluded fields, sensitive fields or fields that are too long are not displayed
+        String[] excludeProperties = {"password", "file"};
+        PropertyPreFilters filters = new PropertyPreFilters();
+        PropertyPreFilters.MySimplePropertyPreFilter excludefilter = filters.addFilter();
+        excludefilter.addExcludes(excludeProperties);
+        LOG.info("Request Parameters: {}", JSONObject.toJSONString(arguments, excludefilter));
+    }
+
+    @Around("controllerPointcut()")
+    public Object doAround(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        long startTime = System.currentTimeMillis();
+        Object result = proceedingJoinPoint.proceed();
+        // Excluded fields, sensitive fields or fields that are too long are not displayed
+        String[] excludeProperties = {"password", "file"};
+        PropertyPreFilters filters = new PropertyPreFilters();
+        PropertyPreFilters.MySimplePropertyPreFilter excludefilter = filters.addFilter();
+        excludefilter.addExcludes(excludeProperties);
+        LOG.info("Result: {}", JSONObject.toJSONString(result, excludefilter));
+        LOG.info("Time Consuming：{} ms ", System.currentTimeMillis() - startTime);
+        LOG.info("------------- Finish -------------", System.currentTimeMillis() - startTime);
+        return result;
+    }
+
+}
