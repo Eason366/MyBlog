4091a12bbb4aa5b4dd9ca574e5e7e53864507416
Eason-Wangy366 <754459450@qq.com>
2022-06-04 05:59:11 -0400

    Add a login box to the front end

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -1,6 +1,9 @@
 <template>
   <a-layout-header class="header" :style="{height:'0.64rem', position: 'fixed', zIndex: 1, width: '100%'}">
     <div class="logo" :style="{}"> Eason's Blog </div>
+    <a class="login-menu" @click="showLoginModal">
+      <span>Login</span>
+    </a>
     <a-menu
         theme="dark"
         mode="horizontal"
@@ -23,14 +26,65 @@
       </a-menu-item>
 
     </a-menu>
+
+    <a-modal
+        title="Login"
+        v-model:visible="loginModalVisible"
+        :confirm-loading="loginModalLoading"
+        ok-text="Login"
+        @ok="login"
+    >
+      <a-form :model="loginUser" :label-col="{ span: 6 }" :wrapper-col="{ span: 18 }">
+        <a-form-item label="User Name">
+          <a-input v-model:value="loginUser.loginName"
+                   placeholder="Please enter Your User Name"/>
+        </a-form-item>
+        <a-form-item label="Password">
+          <a-input-password v-model:value="loginUser.password" type="password"
+                            show-count :maxlength="20"
+                            placeholder="Please enter Your Password"/>
+        </a-form-item>
+      </a-form>
+    </a-modal>
+
   </a-layout-header>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 
 export default defineComponent({
-  name: 'the-header'
+  name: 'the-header',
+  setup () {
+    const loginModalVisible = ref(false);
+    const loginModalLoading = ref(false);
+
+    const loginUser = ref({
+      loginName: "test",
+      password: "test"
+    });
+
+    const showLoginModal = () => {
+      loginModalVisible.value = true;
+    };
+
+
+    const login = () => {
+      console.log("开始登录")
+    };
+
+
+
+
+
+    return {
+      loginModalVisible,
+      loginModalLoading,
+      showLoginModal,
+      loginUser,
+      login
+    }
+  }
 });
 </script>
 
@@ -40,6 +94,14 @@ export default defineComponent({
   height: 0.64rem;
   float: left;
   color: white;
+  font-size: 0.2rem;
+  line-height: 0.64rem;
+}
+.login-menu {
+  float: right;
+  color: white;
+  width: 1.50rem;
+  height: 0.64rem;
   font-size: 0.2rem;
   line-height: 0.64rem;
 }
