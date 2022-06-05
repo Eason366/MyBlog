c35270c6b9fe2ba925a05c2fb44ab137e3dde1d0
Eason-Wangy366 <754459450@qq.com>
2022-06-05 06:50:22 -0400

    integrate redis, Generate token and put in to redis

--- src/main/java/com/eason/blog/controller/BloguserController.java
+++ src/main/java/com/eason/blog/controller/BloguserController.java
@@ -1,6 +1,7 @@
 package com.eason.blog.controller;
 
 
+import com.alibaba.fastjson.JSONObject;
 import com.eason.blog.req.BloguserLoginReq;
 import com.eason.blog.req.BloguserQueryReq;
 import com.eason.blog.req.BloguserSaveReq;
@@ -9,19 +10,32 @@ import com.eason.blog.resp.BloguserQueryResp;
 import com.eason.blog.resp.CommonResp;
 import com.eason.blog.resp.PageResp;
 import com.eason.blog.service.BloguserService;
+import com.eason.blog.util.SnowFlake;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/user")
 public class BloguserController {
+    private static final Logger LOG = LoggerFactory.getLogger(BloguserController.class);
 
     @Resource
     private BloguserService bloguserService;
+
+    @Resource
+    private SnowFlake snowFlake;
+
+    @Resource
+    private RedisTemplate redisTemplate;
+
 
 
     @GetMapping("/all")
@@ -58,6 +72,13 @@ public class BloguserController {
     public CommonResp Login(@Valid @RequestBody BloguserLoginReq req) {
         CommonResp<BloguserLoginResp> resp = new CommonResp<>();
         BloguserLoginResp bloguserLoginResp = bloguserService.login(req);
+
+        Long token = snowFlake.nextId();
+        LOG.info("Generate token：{}，put in to redis", token);
+        bloguserLoginResp.setToken(token.toString());
+        redisTemplate.opsForValue().set(token, JSONObject.toJSONString(bloguserLoginResp), 3600 * 24, TimeUnit.SECONDS);
+
+
         resp.setContent(bloguserLoginResp);
         return resp;
     }
